352f6f7bc52c0e0c2e7068daa33f7bdb732c5466
qcc22362-pixel <qcc22362@gmail.com>
2025-10-27 16:03:30 +0900

    111

--- boot_251027/src/main/java/com/boot/ExamController.java
+++ boot_251027/src/main/java/com/boot/ExamController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+
 @Controller
 @Slf4j
 public class ExamController {
 	@RequestMapping("/")
 //	public void home(){
 	public String home(){
-		log.info("hello boot");
+		log.info("hello boot 111");
 		
 		return "hello";
 	}
